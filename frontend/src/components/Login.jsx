3db5faa1d8474f4f71d6d7931362487d6b7c09b6
Spencer Roberts <spencer7roberts@gmail.com>
2026-05-12 20:34:17 -0400

    Revert "Brand: double topbar + login logo sizes; grow topbar height and login container to match"
    This reverts commit baa6b44367e13d0bfa15aea2a1018ada9b5cdafa.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -25,7 +25,7 @@ export default function Login({ onLogin }) {
     <div className="login-page">
       <div className="login-page-inner">
         <div className="login-brand">
-          <BuildTekLogo size={176} />
+          <BuildTekLogo size={88} />
         </div>
         <div className="login-wrap">
           <h2 style={{marginTop:0}}>Sign in</h2>
